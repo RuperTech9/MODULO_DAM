f5c8d15f1de58dfda817e330084faeeb716581df
alumno <alumno@1DAM-001>
2024-03-11 11:07:26 +0100

    Carga Inicial_2

--- src/tema3/examen/Abecedario.java
+++ src/tema3/examen/Abecedario.java
@@ -1,6 +1,19 @@
 package tema3.examen;
 
+/*
+Crear un procedimiento en java donde se le pase como argumentos el n de elementos que desae visualizar de la serie Fibonacci. 
+La serie comenzara con los 2 ultimos numeros de tu DNI que seran definidos como constantes.
 
+Ejemplo 1, 1
+1,1,2,3,5,8,13,21,34,55,89,144....
+
+Coeficientes a, b, c de una ecuacion de segundo grado, mostrando sus soluciones reales. Si no tiene soluciones reales(numeros complejos del determinante), se indicara mediante un texto abusivo a tal circustancia.
+ax"2 + bx + c=0
+
+Imprime por pantalla el abecedario español.
+QAcabara con un punto(.)
+Tendra las opciones M mayusculas m minusculas y S o s para salir
+*/
 import java.util.Scanner;
 
 /**
