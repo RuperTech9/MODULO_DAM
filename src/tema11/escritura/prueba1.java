08a45b9baae87fb3cb930acf3ca86c030d9583a4
alumno <alumno@1DAM-001>
2024-04-04 13:28:04 +0200

    Carga Inicial_2

--- src/tema11/escritura/prueba1.java
+++ src/tema11/escritura/prueba1.java
@@ -1,8 +1,12 @@
 
 package tema11.escritura;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.util.Scanner;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 /**
@@ -14,10 +18,10 @@ public class prueba1 {
         Scanner sc = new Scanner(System.in);
         File fichero1 = new File(".\\src\\tema11\\escritura\\prueba1.txt");
         
-        //Compruebo que el directorio existe
+        //Compruebo que el directorio existe.
         if(!fichero1.exists()){
             System.out.println("El archivo no existe!!!");
-            System.out.println("¿Quiere crear un nuevo fichero? Pulse Y.");
+            System.out.print("¿Quiere crear un nuevo fichero? Pulse Y.");
             String respuesta = sc.nextLine();
             if(respuesta.equalsIgnoreCase("Y")){
                 System.out.println("Nombre del nuevo fichero: ");
@@ -25,12 +29,42 @@ public class prueba1 {
                 
                 File ficheroUser = new File(".\\src\\tema11\\escritura\\" + nombreFichero);
                 
-                ficheroUser.createNewFile();//Crea el fichero
-                ficheroUser.getAbsoluteFile();//Pilla la ruta donde se guarda el fichero
+                //Posibles excepciones de espacio y permisos. InputException o OutputException
+                try {
+                    ficheroUser.createNewFile();//Crea el fichero
+                    ficheroUser.getAbsoluteFile();//Pilla la ruta donde se guarda el fichero
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+                
                 System.out.println("El archivo se ha creado correctamente.");
             } else{
                 System.out.println("El programa se cerrará");
             }//Fin if-else
+        } // PRIMER IF CREACION DEL FICHERO
+        else{
+            try { //usaremos BufferedReader y BufferedWriter
+                BufferedReader br = new BufferedReader(new FileReader(".\\src\\tema11\\escritura\\prueba2.txt")); //Lectura
+                BufferedWriter bw = new BufferedWriter(new FileWriter(".\\src\\tema11\\escritura\\prueba2.txt")); //Escritura
+                
+                //Escribimos en el fichero
+                bw.write("LINEA 1");
+                System.out.println("ESCRIBA SU LINEA 1:");
+                String escritura = sc.nextLine();
+                bw.write(escritura);
+                //Una nueva linea
+                bw.newLine();
+                bw.write("LINEA 2");
+                System.out.println("ESCRIBA SU LINEA 2:");
+                escritura = sc.nextLine();
+                bw.write(escritura);
+                
+                bw.flush();; //Para guardar el fichero
+                
+                
+            }catch(Exception e){
+                
+            }
         }
         
     }//Fin main
