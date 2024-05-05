4f0487991cd003cce3908faa3e5cd53d8c02988c
ruperSLN <ruper1903@gmail.com>
2024-05-05 12:18:06 +0200

    Carga Inicial

---
+++ src/tema12/ejemplos/DatabaseTest.java
@@ -0,0 +1,25 @@
+package tema12.ejemplos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Ruper
+ */
+public class DatabaseTest {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://192.168.0.27:3306/prueba";
+        String user = "alex";
+        String password = "J0selu1s100%";
+        System.out.println("Conectando... ");
+
+        try (Connection conexion = DriverManager.getConnection(url, user, password)) {
+            System.out.println("Conexión exitosa!");
+        } catch (SQLException e) {
+            System.out.println("Error en la conexión: " + e.getMessage());
+        }
+    }
+}
+
